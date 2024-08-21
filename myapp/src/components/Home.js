80fc7c182bdda974d7f8a1bf69382ee1cb777ed1
prayash <prayashb@cybage.com>
2024-08-21 15:27:42 +0530

    third commit

--- myapp/src/components/Home.js
+++ myapp/src/components/Home.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React ,{useEffect} from 'react'
 
 export default function Home() {
+    useEffect (() => {
+      fetch
+    })
     return (
         <div className="container">
             <blockquote>
